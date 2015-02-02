bc6d7cee5a763fdb5830c67a094185e508ae6d06
gj <gj@gj-VirtualBox.(none)>
2015-02-02 17:23:37 +0800

    checkout page fixed

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,14 +6,27 @@ class TransactionsController < ApplicationController
   gon.client_token = generate_client_token
   end
  
+  def create
+    @result = Braintree::Transaction.sale(
+              amount: current_order.subtotal,
+              payment_method_nonce: params[:payment_method_nonce])
+    if @result.success?
+      current_order.! #method to be made
+      redirect_to root_url, notice: "Congraulations! Your transaction has been successfully!"
+    else
+      flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
+      gon.client_token = generate_client_token
+      render :new
+    end
+  end
+
   private
   def generate_client_token
     Braintree::ClientToken.generate
   end
   
-  private
   def check_cart!
-    if order.order_items.blank?
+    if current_order.order_items.blank?
       redirect_to root_url, alert: "Please add some items to your cart before processing your transaction!"
     end
   end
